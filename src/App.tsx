0923f0dbc1cff9fdbf8642d04d7a09cdc2e95032
surya489 <suryajaya4899@gmail.com>
2026-07-23 13:15:53 +0530

    chore: initialize project with Vite, React, TypeScript and required dependencies

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="text-center h-screen justify-center items-center flex text-3xl font-bold">
+      Let's Code
+    </div>
+  )
+}
+
+export default App
